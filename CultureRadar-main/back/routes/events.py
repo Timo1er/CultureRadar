11e97532ed40ddb4f50a622f4c0c931a88e549b4
Timo1er <135318035+Timo1er@users.noreply.github.com>
2026-06-23 11:43:56 +0200

    Update events.py

--- CultureRadar-main/back/routes/events.py
+++ CultureRadar-main/back/routes/events.py
@@ -2,24 +2,6 @@ from extensions import db
 from datetime import date
 from models.user import User, participants
 
-
-class event(db.Model):
-    __tablename__ = "event"
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(500), nullable=False)
-    author = db.Column(db.String(120), nullable=False)
-    date_debut = db.Column(db.Date, nullable=False)
-    date_fin = db.Column(db.Date, nullable=True)
-    genres = db.Column(db.Text)           # Text = illimité
-    description = db.Column(db.Text)
-    cover_image = db.Column(db.String(512))
-    latitude = db.Column(db.Float, nullable=True)
-    longitude = db.Column(db.Float, nullable=True)
-    prix = db.Column(db.String(100))
-    event_url = db.Column(db.String(512))
-    code_postal = db.Column(db.String(10))
-    participants = db.relationship("User", secondary=participants, back_populates="events_participated")
-
     def to_dict(self):
         return {
             "id": self.id,
